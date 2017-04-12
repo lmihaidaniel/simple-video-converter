26d8b73b359110e1853d9788289d4d8441cf5aec
Lacatusu Mihai Daniel <lacatusu.mihai.daniel@gmail.com>
2017-04-12 13:54:17 +0200

    v.0.2.0

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var args = process.argv.slice(2);
 var type = args[0];
 
 function runConverter(videos, output, settings) {
-  fs.ensureDir(path.dirname(output)).then(function(){
+  return fs.ensureDir(output).then(function() {
     converter.multi(settings, videos, function() {
       log.ok("Done");
       process.exit(1);
@@ -44,136 +44,141 @@ var settings = {
 
 inquirer.registerPrompt("directory", require("inquirer-directory"));
 
-
-var chooseSourceFolder = function(){
-  if(type=="--concat"){
-    log.warn('Make sure that the videos selected have the same resolution before merging them');
+var chooseSourceFolder = function() {
+  if (type == "--concat") {
+    log.warn(
+      "Make sure that the videos selected have the same resolution before merging them"
+    );
   }
   inquirer
-  .prompt([
-    {
-      type: "directory",
-      name: "from",
-      message: "Select media files folder source?",
-      basePath: settings.source
-    }
-  ])
-  .then(function(answers) {
-    var dir = (answers.from || settings.source) + path.sep;
-    var files = fs.readdirSync(dir);
-    var filelist=[];
-    
-    files.forEach(function(file) {
-      var filePath = dir + file;
-      var ext = path.extname(file);
-      if (fs.statSync(filePath).isFile()) {
-        if (defaults.extensions.indexOf(ext) > -1) filelist.push(filePath);
+    .prompt([
+      {
+        type: "directory",
+        name: "from",
+        message: "Select media files folder source?",
+        basePath: settings.source
+      }
+    ])
+    .then(function(answers) {
+      var dir = (answers.from || settings.source) + path.sep;
+      var files = fs.readdirSync(dir);
+      var filelist = [];
+
+      files.forEach(function(file) {
+        var filePath = dir + file;
+        var ext = path.extname(file);
+        if (fs.statSync(filePath).isFile()) {
+          if (defaults.extensions.indexOf(ext) > -1) filelist.push(filePath);
+        }
+      });
+      if (filelist.length > 0) {
+        chooseSourceFiles(filelist);
+      } else {
+        log.fail("No supported media files found");
+        chooseSourceFolder();
       }
     });
-    if(filelist.length>0) {
-      chooseSourceFiles(filelist);
-    }else{
-      log.fail('No supported media files found');
-      chooseSourceFolder();
-    }
-  });
-}
+};
 
-var chooseSourceFiles = function(files){
-  inquirer.prompt({
-    type: 'checkbox',
-    name: 'files',
-    message: 'Select the files to convert',
-    choices: files,
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return 'You must choose at least one media file.';
-      }
-      return true;
-    }
-  }).then(function (answers) {
-    chooseSettings(answers.files);
-  });
-}
-
-var chooseSettings = function(files){
+var chooseSourceFiles = function(files) {
   inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "output",
-      message: "Select output directory:",
-      default: settings.output,
-      filter: function(val) {
-        return path.normalize(val);
+    .prompt({
+      type: "checkbox",
+      name: "files",
+      message: "Select the files to convert",
+      choices: files,
+      validate: function(answer) {
+        if (answer.length < 1) {
+          return "You must choose at least one media file.";
+        }
+        return true;
       }
-    },
-    {
-      type: "confirm",
-      name: "defaults",
-      message: "Use default encoding options ?",
-      default: true
-    },
-    {
-      type: "input",
-      name: "resolution",
-      message: "Video resolution:",
-      default: settings.resolution,
-      when: function(answers) {
-        return !answers.defaults;
-      },
-      validate: validators.resolution
-    },
-    {
-      type: "input",
-      name: "quality",
-      message: "Video constant quality [Range 18-31, lowest for better quality]:",
-      default: settings.quality,
-      when: function(answers) {
-        return !answers.defaults;
-      },
-      filter: Number,
-      validate: validators.number
-    },
-    {
-      type: "input",
-      name: "bitrate",
-      message: "Video max bitrate desired:",
-      default: settings.bitrate,
-      when: function(answers) {
-        return !answers.defaults;
-      },
-      validate: validators.bitrate
-    }
-  ])
-  .then(function(answers) {
-    // Get the options and defaults.
-    var options = deepmerge(settings, answers);
+    })
+    .then(function(answers) {
+      chooseSettings(answers.files);
+    });
+};
 
-    var maxrate = Math.ceil(8 * parseFloat(options.bitrate) / 10 - 128);
-    var bufsize = Math.ceil(4 * maxrate / 3);
+var chooseSettings = function(files) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "output",
+        message: "Select output directory:",
+        default: settings.output,
+        filter: function(val) {
+          return path.normalize(val);
+        }
+      },
+      {
+        type: "confirm",
+        name: "defaults",
+        message: "Use default encoding options ?",
+        default: true
+      },
+      {
+        type: "input",
+        name: "resolution",
+        message: "Video resolution:",
+        default: settings.resolution,
+        when: function(answers) {
+          return !answers.defaults;
+        },
+        validate: validators.resolution
+      },
+      {
+        type: "input",
+        name: "quality",
+        message: "Video constant quality [Range 18-31, lowest for better quality]:",
+        default: settings.quality,
+        when: function(answers) {
+          return !answers.defaults;
+        },
+        filter: Number,
+        validate: validators.number
+      },
+      {
+        type: "input",
+        name: "bitrate",
+        message: "Video max bitrate desired:",
+        default: settings.bitrate,
+        when: function(answers) {
+          return !answers.defaults;
+        },
+        validate: validators.bitrate
+      }
+    ])
+    .then(function(answers) {
+      // Get the options and defaults.
+      var options = deepmerge(settings, answers);
 
-    defaults.video.resolution = options.resolution;
-    defaults.video.quality = options.quality;
-    defaults.video.maxrate = maxrate + "k";
-    defaults.video.bufsize = bufsize + "k";
+      var maxrate = Math.ceil(8 * parseFloat(options.bitrate) / 10 - 128);
+      var bufsize = Math.ceil(4 * maxrate / 3);
 
-    
-    var videos = [];
-    if(type!=="--concat"){
-      files.forEach(function(item){
-        var ext = path.extname(item);
-        var out = answers.output + path.sep + path.basename(item, ext) + ".mp4";
-        videos.push([item, path.normalize(out)]);
-      })
-    }else{
-      videos = [[[],answers.output+path.sep+"concat.mp4"]];
-      files.forEach(function(item){
-        videos[0][0].push(item);
-      })
-    }
-    runConverter(videos, answers.output, defaults);
-  });
-}
+      defaults.video.resolution = options.resolution;
+      defaults.video.quality = options.quality;
+      defaults.video.maxrate = maxrate + "k";
+      defaults.video.bufsize = bufsize + "k";
+
+      var videos = [];
+      if (type !== "--concat") {
+        files.forEach(function(item) {
+          var ext = path.extname(item);
+          var out = answers.output +
+            path.sep +
+            path.basename(item, ext) +
+            ".mp4";
+          videos.push([item, path.normalize(out)]);
+        });
+      } else {
+        videos = [[[], answers.output + path.sep + "concat.mp4"]];
+        files.forEach(function(item) {
+          videos[0][0].push(item);
+        });
+      }
+      runConverter(videos, answers.output, defaults);
+    });
+};
 
 chooseSourceFolder();
